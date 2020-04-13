3b628913dd99152ff5b330b02c6bf61b79a651ec
JulianChambrier <60175779+JulianChambrier@users.noreply.github.com>
2020-04-13 18:23:55 +0200

    Add files via upload

---
+++ test/construction_index_inverse_test.py
@@ -0,0 +1,24 @@
+"""
+@author: Julian CHAMBRIER
+
+"""
+
+#Importation des bibliothèques utiles
+import inverted_index_test as ii
+import cleaner_test
+
+#Main
+if __name__ == '__main__':
+    #On charge les documents
+    documents = cleaner_test.loadURL("./pages_web_test")
+    #On transforme les documents en un dictionnaire {'file_name':'file_content'}
+    documents_cleaner = cleaner_test.convert_url(documents)
+    #On crée un index inversé
+    stop_words = []
+    index_inverse = ii.inverted_index(stop_words)
+    #On remplit l'index inversé avec les documents fournis
+    index_inverse.build(documents_cleaner)
+    #On affiche l'index inversé
+    index_inverse.display()
+
+ 
