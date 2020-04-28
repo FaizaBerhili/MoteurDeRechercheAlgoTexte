26318f394af01c0e1dbed10dfb4b78edc1a5a60c
EmilianoBoussac <52208904+EmilianoBoussac@users.noreply.github.com>
2020-04-28 11:29:10 +0200

    Add files via upload

--- src/motSimilaire.py
+++ src/motSimilaire.py
@@ -6,7 +6,34 @@ import textdistance
 
 def motSimilaire(motCle,Dico):
 	liste = []
-	for keys in Dico.keys():
-		if textdistance.levenshtein(motCle,keys)<2 :
-			liste.append(keys)
+	if(len(motCle)>2):
+		for keys in Dico.keys():
+			if textdistance.levenshtein(motCle,keys)<2 :
+				liste.append(keys)
+	else:
+		liste.append(motCle)
 	return liste
+
+
+
+
+# a mettre au debut de la fonction search de index inversé
+# request : liste de string ["le","chat","noir"]
+
+def listeSimilaire2(request,index):
+	request2=[]
+	for word in request:
+		request2.append(motSimilaire(word,index))
+	return request2
+
+def listeSimilaire(request,Dico):
+	liste = []
+	for motCle in request:
+		if(len(motCle)>2):
+			for keys in Dico.keys():
+				if textdistance.levenshtein(motCle,keys)<2 :
+					liste.append(keys)
+		else:
+			liste.append(motCle)
+	return liste
+
