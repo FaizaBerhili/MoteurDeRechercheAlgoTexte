9cfb3c6099a02d4e6629285e2f57f92ddb902053
EmilianoBoussac <52208904+EmilianoBoussac@users.noreply.github.com>
2020-05-07 12:00:38 +0200

    Add files via upload

---
+++ src/supprimeSimilaireSRC.py
@@ -0,0 +1,28 @@
+#Emiliano BOUSSAC
+
+import sys
+import numpy as np
+import re
+import math
+from collections import Counter
+import cleaner_test
+import textdistance
+import shutil
+import os
+import supprimeSimilaire as supSim
+from pageSimilaireSRC import dico
+
+
+fichiers = os.listdir('./pages_web_test/');
+print("nombre fichiers avant suppresssion");
+print(len(fichiers));
+
+
+
+supSim.supressionPage(dico);
+
+fichiers = os.listdir('./pages_web_test/');
+print("nombre fichiers apres suppresssion");
+print(len(fichiers));
+
+
