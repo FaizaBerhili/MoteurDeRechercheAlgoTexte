dd13328a64bedab9985164f41f84b82eec96ed93
EmilianoBoussac <52208904+EmilianoBoussac@users.noreply.github.com>
2020-05-07 14:44:40 +0200

    Add files via upload

--- src/pageSimilaireSRC.py
+++ src/pageSimilaireSRC.py
@@ -5,14 +5,15 @@ import numpy as np
 import re
 import math
 from collections import Counter
-import cleaner
+import cleaner_test
 import pageSimilaire as pS
 
-documents = cleaner.loadURL("./pages_web");
-documents_cleaner = cleaner.convert_url(documents);
+documents = cleaner_test.loadURL("./pages_web_test");
+documents_cleaner = cleaner_test.convert_url(documents);
 
 dico = pS.dicoSimilaire(documents_cleaner);
 
+print(dico)
 
 for lien1,liens2 in dico.items():	
 	print("la page \"" , lien1 , "\"");
--- src/supprimeSimilaire.py
+++ src/supprimeSimilaire.py
@@ -5,7 +5,7 @@ import numpy as np
 import re
 import math
 from collections import Counter
-import cleaner
+import cleaner_test
 import textdistance
 import shutil
 import os
@@ -14,7 +14,7 @@ import os
 
 
 def supressionPage(dico):
-	fichiers = os.listdir('./pages_web/');
+	fichiers = os.listdir('./pages_web_test/');
 	n = len(fichiers);
 	compteur = 0 ;
 	for fichier in fichiers:
@@ -23,15 +23,15 @@ def supressionPage(dico):
 		print("verifie si fichier" , fichier ," est dans dico");
 		sys.stdout.flush()
 		try:
-			#print(dico[fichier]) #KeyError
+			print(dico[fichier]); #KeyError
 			#si dans le dico alors supprimé du dico les pages dans son set
 			setfichiers = dico[fichier]; #set de nom fichier (string)
-			#print("clé trouvé")
+			print("clé trouvé")
 			for f in setfichiers:
 				dico.pop(f); #supprime clé nom du fichier du dico
 		except KeyError:
-			#print("pas de clé trouvé donc suppression de la page")
-			os.remove('./pages_web/' + fichier);
+			print("pas de clé trouvé donc suppression de la page")
+			os.remove('./pages_web_test/' + fichier);
 
 
 
