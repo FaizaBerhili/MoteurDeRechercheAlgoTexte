e430c4533099eae6427b9d99780301c082efc512
JulianChambrier <60175779+JulianChambrier@users.noreply.github.com>
2020-05-09 14:12:15 +0200

    Update supprimeSimilaire.py

--- test/supprimeSimilaire.py
+++ test/supprimeSimilaire.py
@@ -9,29 +9,28 @@ import cleaner_test
 import textdistance
 import shutil
 import os
+import progress_bar_test
 
 
 
 
 def supressionPage(dico):
-	fichiers = os.listdir('./pages_web_test/');
-	n = len(fichiers);
-	compteur = 0 ;
-	for fichier in fichiers:
-		compteur = compteur + 1;
-		print(str(compteur) , " / " , str(n));
-		print("verifie si fichier" , fichier ," est dans dico");
-		sys.stdout.flush()
-		try:
-			print(dico[fichier]); #KeyError
-			#si dans le dico alors supprimé du dico les pages dans son set
-			setfichiers = dico[fichier]; #set de nom fichier (string)
-			print("clé trouvé")
-			for f in setfichiers:
-				dico.pop(f); #supprime clé nom du fichier du dico
-		except KeyError:
-			print("pas de clé trouvé donc suppression de la page")
-			os.remove('./pages_web_test/' + fichier);
+    fichiers = os.listdir('./pages_web_test/')
+    taille = len(fichiers)
+    compteur = 1
+    for fichier in fichiers:
+	    try:
+	        #print(dico[fichier]); #KeyError
+		    #si dans le dico alors supprimé du dico les pages dans son set
+		    setfichiers = dico[fichier] #set de nom fichier (string)
+		    #print("clé trouvé")
+		    for f in setfichiers:
+		        dico.pop(f) #supprime clé nom du fichier du dico
+	    except KeyError:
+	     	#print("pas de clé trouvé donc suppression de la page")
+	     	os.remove('./pages_web_test/' + fichier)
+	    progress_bar_test.print_progress_bar(compteur, taille, prefix = 'Vérifie si les fichiers sont dans le dico : ' + str(compteur) +  '/' + str(taille), suffix = '')
+	    compteur = compteur + 1
 
 
 
