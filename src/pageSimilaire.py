132e8d2e401da331f06e17b55d561270a20b6a64
JulianChambrier <60175779+JulianChambrier@users.noreply.github.com>
2020-05-09 14:16:56 +0200

    Update pageSimilaire.py

--- src/pageSimilaire.py
+++ src/pageSimilaire.py
@@ -5,6 +5,7 @@ import re
 import math
 from collections import Counter
 import textdistance
+import progress_bar
 
 
 #prend en entrée le dico créer par le cleaner avec comme clé le lien de la page et comme valeur
@@ -13,21 +14,20 @@ import textdistance
 #et les valeurs sont un set des liens des pages avec lesquelles ils sont similaires
 
 def dicoSimilaire(dicoDocuments):
-	print("creation du dico des pages similaires");
+	print("Creation du dico des pages similaires");
 	dicoSim = dict()
-	compteur = 0
+	compteur = 1
 	taille = len(dicoDocuments.items());
-	print("traitement pages similaires")
+	print("Traitement pages similaires\n")
 	for lien1,texte1 in dicoDocuments.items():
-		compteur = compteur + 1
-		print(str(compteur)," / ",taille,"\n")
 		for lien2,texte2 in dicoDocuments.items():
-		
 			if(lien1 != lien2):
 				d = textdistance.hamming.similarity(texte1,texte2)
 				s = dicoSim.setdefault(lien1,set())				
 				if(d>300): # si leur similarité est supérieur à 300 on ajoute la page comme valeur dans le set
 					s.add(lien2)
+		progress_bar.print_progress_bar(compteur, taille, prefix = 'Vérifie si les fichiers sont dans le dico : ' + str(compteur) +  '/' + str(taille), suffix = '')
+		compteur = compteur + 1
 	return (dicoSim)
 	
 
