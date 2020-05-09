06c4650714b990c557a92a80cb82dc2d66afabbf
JulianChambrier <60175779+JulianChambrier@users.noreply.github.com>
2020-05-09 14:19:08 +0200

    Update supprimeSimilaire.py

--- src/supprimeSimilaire.py
+++ src/supprimeSimilaire.py
@@ -9,29 +9,29 @@ import cleaner
 import textdistance
 import shutil
 import os
+import progress_bar
 
 
 
 
 def supressionPage(dico):
 	fichiers = os.listdir('./pages_web/');
-	n = len(fichiers);
-	compteur = 0 ;
+	taille = len(fichiers)
+	compteur = 1
 	for fichier in fichiers:
-		compteur = compteur + 1;
-		print(str(compteur) , " / " , str(n));
-		print("verifie si fichier" , fichier ," est dans dico");
-		sys.stdout.flush()
 		try:
-			print(dico[fichier]); #KeyError
+	    	#print(dico[fichier]); #KeyError
 			#si dans le dico alors supprimé du dico les pages dans son set
-			setfichiers = dico[fichier]; #set de nom fichier (string)
-			print("clé trouvé")
+			#set de nom fichier (string)
+			setfichiers = dico[fichier] 
+			#print("clé trouvé")
 			for f in setfichiers:
 				dico.pop(f); #supprime clé nom du fichier du dico
 		except KeyError:
-			print("pas de clé trouvé donc suppression de la page")
-			os.remove('./pages_web/' + fichier);
+	    	#print("pas de clé trouvé donc suppression de la page")
+			os.remove('./pages_web/' + fichier)
+		progress_bar.print_progress_bar(compteur, taille, prefix = 'Vérifie si les fichiers sont dans le dico : ' + str(compteur) +  '/' + str(taille), suffix = '')
+		compteur = compteur + 1
 
 
 
