03027e5b54d7354cde1e4b3387ee1463879a08d7
JulianChambrier <60175779+JulianChambrier@users.noreply.github.com>
2020-05-09 14:10:49 +0200

    Add files via upload

---
+++ test/progress_bar_test.py
@@ -0,0 +1,36 @@
+"""
+@author CHAMBRIER Julian
+
+"""
+import os
+def print_progress_bar (it, taille, prefix = '', suffix = '', logo = '█', printEnd = "\r"):
+    """
+    Affiche une bar de progression
+    @params:
+        it  - Required  : interation de boucle (Int)
+        taille - Required  : taille totale de l'itération (Int)
+        prefix - Optional  : Ce que l'on veut écrire avant la barre de progression (Str)
+        suffix - Optional  : Ce que l'on veut écrire après la barre de progression(Str)
+        logo   - Optional  : Logo de progression (Str)
+        printEnd - Optional  : Caractere de fin de progress bar (Str)
+    """
+    #Affichage du pourcentage sans chiffre après la virgule
+    percent = ("{0:.0f}").format(100 * (it / float(taille)))
+    #Calcule de la taille du texte pour rester sur une ligne du terminal
+    tailletext = len(prefix) + len(suffix) + 12
+    #La taille de la progress bar s'ajuste en fonction de la grandeur du terminal
+    #Si on agrandi ou rétrécit le terminal la progress bar s'ajustera
+    taillelogo = int((os.get_terminal_size().columns - tailletext) * it // taille)
+    #Affichage progress bar
+    progress_bar = logo * taillelogo + '-' * ((os.get_terminal_size().columns - tailletext) - taillelogo)
+    print('\r%s |%s| %s%% %s' % (prefix, progress_bar, percent, suffix), end = printEnd)
+    # On affiche une nouvelle ligne quand le chargement est terminé 
+    if it == taille: 
+        print()
+
+#Main de test
+if __name__ == '__main__':
+    #Test de la progresse bar 
+    for i in range(10001):
+        print_progress_bar(i,10000, prefix ="Progression : "+ str(i) + '/' + str(10000), suffix = '')
+ 
