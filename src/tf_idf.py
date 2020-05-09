281711dcd16df8c46df888d9b1b819f4b66c41a2
JulianChambrier <60175779+JulianChambrier@users.noreply.github.com>
2020-05-09 14:20:13 +0200

    Update tf_idf.py

--- src/tf_idf.py
+++ src/tf_idf.py
@@ -1,3 +1,8 @@
+"""
+@author Julian CHAMBRIER
+
+"""
+
 #Importation des bibliothèques utiles
 from os import listdir
 from os.path import isfile, join
@@ -11,27 +16,65 @@ import sys
 import functools
 import numpy
 
-#Fonction Term Frequency avec la fréquence brute (nombre d'occurence du term / taille du texte)
 def TF_frequence_brute(term , document_content):
-    document_split = document_content.split()
-    return document_split.count(term)/len(document_split)
+    """
+    Fonction Term Frequency avec la fréquence brute (nombre d'occurence du term / taille du texte)
 
-#Fonction Term Frequency binaire(nombre d'occurence dans le texte)
+    @params:
+        term - Required : Le terme dont on veut calculer le score (Str)
+        document_content  - Required : Le document sur lequel on calcul le score (Str)
+    """
+    document_split = document_content.split()
+    try: 
+        return document_split.count(term)/len(document_split)
+    except:
+        #Division par 0
+        return 0
+
 def TF_binaire(term , document_content):
+    """
+    Fonction Term Frequency binaire(nombre d'occurence dans le texte)
+
+    @params:
+        term - Required : Le terme dont on veut calculer le score (Str)
+        document_content  - Required : Le document sur lequel on calcul le score (Str)
+    """
     return document_content.split().count(term)
 
-#Fonction Term Frequecy avec la normalisation logarithmique (1 + log((nombre d'occurence du term / taille du texte)))
 def TF_normalisation_log(term , document_content):
+    """
+    Fonction Term Frequecy avec la normalisation logarithmique (1 + log((nombre d'occurence du term / taille du texte)))
+
+    @params:
+        term - Required : Le terme dont on veut calculer le score (Str)
+        document_content  - Required : Le document sur lequel on calcul le score (Str)
+    """
     return 1 + math.log10(TF_frequence_brute(term,document_content))
 
-#Fonction Term Frequency avec normalisation 0.5 par le max
 def TF_normalisation_0_5_max(term,document_content):
+    """
+    Fonction Term Frequency avec normalisation 0.5 par le max
+
+    @params:
+        term - Required : Le terme dont on veut calculer le score (Str)
+        document_content  - Required : Le document sur lequel on calcul le score (Str)
+    """
     document_split = document_content.split()
     frequency_list = [TF_frequence_brute(word,document_content) for word in document_split]
-    return 0.5 + 0.5 * ( TF_frequence_brute(term,document_content) / max(frequency_list))
+    try:
+        return 0.5 + 0.5 * ( TF_frequence_brute(term,document_content) / max(frequency_list))
+    except:
+        #division par 0
+        return 0.5
 
-#Fonction Inverse Document Frequency 
 def IDF(term , documents_contents):
+    """
+    Fonction Inverse Document Frequency 
+
+    @params:
+        term - Required : Le terme dont on veut calculer le score (Str)
+        documents_contents  - Required : Les documents sur lesquels on veut calculer le score (List of Str)
+    """
     doc_contain_term = 0
     for document_content in documents_contents:
         document_split = document_content.split()
@@ -41,7 +84,7 @@ def IDF(term , documents_contents):
         return math.log10( len(documents_contents) / doc_contain_term )
     except:
         #Division par 0
-        return sys.maxsize
+        return 0
 """
 #Autre manière de calculer l'IDF
 def IDF_2(term , documents_contents):
@@ -57,8 +100,17 @@ def IDF_2(term , documents_contents):
         #Division par 0
         return sys.maxsize
 """
-#Fonction calculant le TF_IDF
-#Pour des raisons de praticité et vu le nombre de fonctions TF différentes, il est plus simple que TF_IDF prenne en argument une fonction TF.
-#Ainsi l'utilisateur va pouvoir choisir la fonction qu'il souhaite utiliser
+
 def TF_IDF(TF_function,term, document_content, documents_contents):
+    """
+    Fonction calculant le TF_IDF
+    Pour des raisons de praticité et vu le nombre de fonctions TF différentes, il est plus simple que TF_IDF prenne en argument une fonction TF.
+    Ainsi l'utilisateur va pouvoir choisir la fonction qu'il souhaite utiliser 
+
+    @params:
+        TF_function - Required : La fonction de calcul de TF à utiliser (fonction)
+        term - Required : Le terme dont on veut calculer le score (Str)
+        document_content  - Required : Le document sur lesquels on veut calculer le score d'où est extrait le term (List of Str)
+        documents_contents  - Required : Les documents sur lesquels on veut calculer le score (List of Str)
+    """
     return TF_function(term, document_content) * IDF(term,documents_contents)
